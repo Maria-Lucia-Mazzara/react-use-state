553b19580086e1482426615a8fa9c56340160a23
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-07 15:58:57 +0100

    aggiunto card e Nav bar

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,52 +1,11 @@
-
+import Card from './Card'
+import Nav from './Nav'
 function Main() {
     return (
         <main>
             <section className="container">
-                <div className="row mt-4">
-                    <ul className="nav nav-pills gap-2">
-                        <li className="nav-item">
-                            <a className="nav-link active bg-warning text-dark" href="#">
-                                HTML
-                            </a>
-
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link bg-primary text-light" href="#">
-                                CSS
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link bg-primary text-light" href="#">
-                                JavaScript
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link bg-primary text-light" href="#">
-                                Node.js
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link bg-primary text-light" href="#">
-                                Express
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link bg-primary text-light" href="#">
-                                React.js
-                            </a>
-                        </li>
-                        <div className="card card-body mt-4">
-                            <h2>
-                                HTML
-                            </h2>
-                            <p className="fs-4">
-                                HTML (HyperText Markup Language) è il linguaggio standard per creare pagine
-                                e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini.
-                            </p>
-                        </div>
-                    </ul>
-                </div>
+                <Nav />
+                <Card />
             </section>
         </main>
     )
