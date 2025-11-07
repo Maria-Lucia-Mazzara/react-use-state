7a1ab0e75296b4424d37a03fdc97512af39b651c
Maria-Lucia-Mazzara <marialuciaboolen@gmail.com>
2025-11-07 17:41:34 +0100

    fatta parte useState

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,12 +1,69 @@
-import Card from './Card'
-import Nav from './Nav'
+import { useState } from 'react';
+
+const languages = [
+    {
+        id: 1,
+        title: "HTML",
+        description: "HTML (HyperText Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini."
+    },
+    {
+        id: 2,
+        title: "CSS",
+        description: "CSS (Cascading Style Sheets) è un linguaggio di stile utilizzato per descrivere la presentazione di un documento scritto in HTML o XML. Controlla il layout, i colori, i caratteri e l'aspetto complessivo di una pagina web."
+    },
+    {
+        id: 3,
+        title: "JavaScript",
+        description: "JavaScript è un linguaggio di programmazione dinamico utilizzato per lo sviluppo web, che consente contenuti interattivi come grafici animati, pulsanti cliccabili e altre funzionalità dinamiche sui siti web."
+    },
+    {
+        id: 4,
+        title: "Node.js",
+        description: "Node.js è un ambiente di runtime JavaScript lato server che permette di utilizzare JavaScript anche sul backend, consentendo agli sviluppatori di usare lo stesso linguaggio sia sul client che sul server."
+    },
+    {
+        id: 5,
+        title: "Express",
+        description: "Express è un framework per applicazioni web minimalista e veloce per Node.js. Fornisce strumenti robusti per creare applicazioni web e API e semplifica la gestione delle rotte e delle richieste."
+    },
+    {
+        id: 6,
+        title: "ReactJS",
+        description: "ReactJS è una libreria JavaScript per costruire interfacce utente, in particolare applicazioni a pagina singola. Consente agli sviluppatori di creare componenti UI riutilizzabili e di gestire efficacemente lo stato dell'applicazione."
+    }
+];
+
 function Main() {
+
+    const [id, setid] = useState(1);
+
+    function bottone(selezionato) {
+        setid(selezionato);
+    }
+
     return (
-        <main>
-            <section className="container">
-                <Nav />
-                <Card />
+        <main className="container">
+            <section>
+                <div className="row mt-4 ">
+                    <ul className="nav nav-pills gap-2">
+                        {languages.map((titolo) => (
+                            <li className="nav-item">
+                                <a className="nav-link active bg-warning text-dark" key={titolo.id} onClick={() => bottone(titolo.id)} href="#">
+                                    {titolo.title}
+                                </a>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
             </section>
+            <section>
+                <div className="card">
+                    <div className="card-body">
+                        {languages[id - 1].description}
+                    </div>
+                </div>
+            </section>
+
         </main>
     )
 }
